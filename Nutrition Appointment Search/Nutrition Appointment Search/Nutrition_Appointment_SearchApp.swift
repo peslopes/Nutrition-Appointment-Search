9cf6be5e1e92068687278d5baacd11d4eff8221b
Pedro Lopes <pedroesl96@gmail.com>
2025-03-06 12:28:36 +0000

    Initial commit

---
+++ Nutrition Appointment Search/Nutrition Appointment Search/Nutrition_Appointment_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nutrition_Appointment_SearchApp.swift
+//  Nutrition Appointment Search
+//
+//  Created by Pedro Lopes on 06/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Nutrition_Appointment_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
